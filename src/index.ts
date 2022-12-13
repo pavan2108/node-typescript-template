86dfd19e40e722c93de49229db8154218baaecda
pavan2108 <pavansssai2001@gmail.com>
2022-12-14 00:32:16 +0530

    Added db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World');
+import express from 'express';
+import dotenv from "dotenv"
+import { graphqlHTTP } from 'express-graphql';
+
+import { root } from './Schema/schema';
+import connect from './Config/database';
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: root,
+        graphiql: process.env.NODE_ENV === "development"
+    })
+);
+connect();
+app.listen(PORT, () => console.log(`Now browse to localhost:${PORT}/graphql`));
